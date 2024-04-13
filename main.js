c88710d2c543eb5b8a46b237869546d146236b87
Elmurat <110521844+adept02@users.noreply.github.com>
2024-04-13 22:45:51 +0500

    project

---
+++ main.js
@@ -0,0 +1,45 @@
+var math = document.getElementById("math");
+
+function display(num) {
+  math.value += num;
+}
+
+function calculate() {
+  try {
+    math.value = eval(math.value);
+  }
+  catch (err) {
+    alert("!!!")
+  }
+}
+
+// document.addEventListener('keypress', (event) => {
+//   if(event.key === 'Enter'){
+//     calculate()
+//   }
+// })
+
+function clean() {
+  math.value = '';
+}
+
+function zero() {
+   math.value = 0;
+}
+
+function roots() {
+   math.value = math.value ** 0.5
+}
+
+function sqr() {
+   math.value *= math.value;
+}
+
+function mod() {
+   math.value /= 100
+}
+
+function del() {
+  math.value = math.value.slice(0, -1);
+
+}
